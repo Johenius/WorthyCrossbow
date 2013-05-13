f560054afe0e89402a721b5431546226abd346a3
John Richter <j.richter@ru.ac.za>
2013-05-13 15:35:36 +0200

    Empty rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorthyCrossbow::Application.config.secret_token = 'ee284a39047b7d8c7ec027a19618317f3a8cee7ff7c09bcaef840f796349a95bf901edc326590dbb121457ee2ea76b26a2a449f2b813738589d858daeae2fbf2'
